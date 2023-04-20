7c4aaf607082676b7475f63ef143d889fd8b13e5
Artyom Pionov <pionovartem11@gmail.com>
2023-04-20 22:17:04 +0100

    First Commit: Starting module 14. TODO: Solve problem.

---
+++ src/Strange_Bank_Problem/Bank.java
@@ -0,0 +1,8 @@
+package Strange_Bank_Problem;
+
+/* TODO: Try creating a system like this:
+ * A Bank class, which has an integer variable money.
+ * And Client class, a subclass of Thread class,
+ * which in infinite loop takes $1000 loan from bank (decrease money) and immediately returns (increase money).
+ */
+public class Bank {}
