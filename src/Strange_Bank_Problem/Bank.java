fff0e673d1c161e5f6fe6026531cbd1257c1fb92
Artyom Pionov <pionovartem11@gmail.com>
2023-04-22 21:43:26 +0100

    New Commit: Further learning about multithreading and synchronization of threads.

--- src/Strange_Bank_Problem/Bank.java
+++ src/Strange_Bank_Problem/Bank.java
@@ -1,8 +1,55 @@
 package Strange_Bank_Problem;
 
-/* TODO: Try creating a system like this:
+/* Created a system where:
  * A Bank class, which has an integer variable money.
  * And Client class, a subclass of Thread class,
- * which in infinite loop takes $1000 loan from bank (decrease money) and immediately returns (increase money).
+ * in which infinite loop takes $1000 loan from bank (decrease money) and immediately returns (increase money).
  */
-public class Bank {}
+
+@SuppressWarnings({"BusyWait", "InfiniteLoopStatement", "SynchronizeOnNonFinalField", "SameParameterValue", "FieldMayBeFinal", "unused"})
+public class Bank {
+
+    private int money = 10000;
+    private Object lock = new Object();
+
+    int getMoney() {
+        return money;
+    }
+
+    void take(int money) {
+        synchronized (lock) {
+            this.money -= money;
+        }
+    }
+
+    void repay(int money) {
+        synchronized (lock) {
+            this.money += money;
+        }
+    }
+
+    class Client extends Thread{
+        @Override
+        public void run() {
+            while(true) {
+                take(1000);
+                repay(1000);
+            }
+        }
+    }
+
+    public Bank() {
+        new Client().start();
+        new Client().start();
+        new Client().start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Bank bank = new Bank();
+        while(true) {
+            System.out.println(bank.money);
+            Thread.sleep(1000);
+        }
+    }
+
+}
